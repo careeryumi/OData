e13e75e2b7c09a8fcdc118ba54fb75c829f7dfc9
Yumi <careeryumi@gmail.com>
2021-12-02 14:51:46 -0500

    initial

---
+++ MusicLibraryServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MusicLibraryServices.MvcApplication" Language="C#" %>
---
+++ MusicLibraryServices/WcfDataService1.svc.cs
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using MusicLibraryDB;
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Data.Services.Providers;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace MusicLibraryServices
+{
+    public class WcfDataService1 : EntityFrameworkDataService<MusicDBEntities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            config.SetEntitySetAccessRule("Musics", EntitySetRights.AllRead);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+    }
+}
